a71f672a011306d5d7451790a45dfe18ffd8537d
KhushhalTravelxp <khushhal.sharma@travelxp.tv>
2025-02-17 15:37:49 +0530

    added token

--- src/app/page.js
+++ src/app/page.js
@@ -20,19 +20,20 @@ export default function ShakaPlayer() {
 
       playerRef.current.getNetworkingEngine().registerRequestFilter((type, request) => {
         if (type === shaka.net.NetworkingEngine.RequestType.LICENSE) {
-          request.headers["X-AxDRM-Message"] = "";
+          request.headers["X-AxDRM-Message"] =
+            "eyJhbGciOiJIUzI1NiJ9.eyJ2ZXJzaW9uIjoxLCJjb21fa2V5X2lkIjoiYjQ1ODc2N2QtYTgzYi00MWQ0LWFlNjgtYWNhNzAwZDNkODRmIiwibWVzc2FnZSI6eyJ0eXBlIjoiZW50aXRsZW1lbnRfbWVzc2FnZSIsInZlcnNpb24iOjIsImxpY2Vuc2UiOnsiZXhwaXJhdGlvbl9kYXRldGltZSI6IjIwMjUtMDItMThUMDM6NDQ6MDAuODIxWiIsImFsbG93X3BlcnNpc3RlbmNlIjp0cnVlLCJyZWFsX3RpbWVfZXhwaXJhdGlvbiI6dHJ1ZX0sImNvbnRlbnRfa2V5X3VzYWdlX3BvbGljaWVzIjpbeyJuYW1lIjoiUG9saWN5IEEiLCJ3aWRldmluZSI6eyJkZXZpY2Vfc2VjdXJpdHlfbGV2ZWwiOiJTV19TRUNVUkVfQ1JZUFRPIn19XSwiY29udGVudF9rZXlzX3NvdXJjZSI6eyJpbmxpbmUiOlt7ImlkIjoiMTAyM2JhMThiZTMzYWE4YTg2MTFlZWE4OGNkZWQ5ZTMiLCJ1c2FnZV9wb2xpY3kiOiJQb2xpY3kgQSJ9LHsiaWQiOiJjY2I0ZjJmYjExNTVmN2JmYmIwN2U3Yzc1ZDI2YTY3YSIsInVzYWdlX3BvbGljeSI6IlBvbGljeSBBIn0seyJpZCI6IjU4ODE5M2VjZjhjMzc5OGY2MjMxYjM1N2M0N2YwYWJiIiwidXNhZ2VfcG9saWN5IjoiUG9saWN5IEEifV19fSwiaWF0IjoxNzM5NzY3NDQwLCJleHAiOjE3Mzk4NTM4NDB9.VhZA2UrSXAR52syUJ-deu8WNAsT65RMJDbDIdogKx4M";
         }
       });
 
       const drmConfig = {
-        "com.apple.fps": ""
+        "com.apple.fps": "https://c8eaeae1-drm-fairplay-licensing.axprod.net/AcquireLicense"
       };
       playerRef.current.configure({
         drm: {
           servers: drmConfig,
           advanced: {
             "com.apple.fps": {
-              serverCertificateUri: ""
+              serverCertificateUri: "https://travelxp.akamaized.net/cert/fairplay/fairplay.cer"
             }
           }
         },
