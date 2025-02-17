08cc1f2a6b6326640e75c30ef63152943203fa5c
KhushhalTravelxp <khushhal.sharma@travelxp.tv>
2025-02-17 15:31:32 +0530

    added streaming

--- src/app/page.js
+++ src/app/page.js
@@ -20,44 +20,44 @@ export default function ShakaPlayer() {
 
       playerRef.current.getNetworkingEngine().registerRequestFilter((type, request) => {
         if (type === shaka.net.NetworkingEngine.RequestType.LICENSE) {
-          request.headers["X-AxDRM-Message"] =
-            "eyJhbGciOiJIUzI1NiJ9.eyJ2ZXJzaW9uIjoxLCJjb21fa2V5X2lkIjoiYjQ1ODc2N2QtYTgzYi00MWQ0LWFlNjgtYWNhNzAwZDNkODRmIiwibWVzc2FnZSI6eyJ0eXBlIjoiZW50aXRsZW1lbnRfbWVzc2FnZSIsInZlcnNpb24iOjIsImxpY2Vuc2UiOnsiZXhwaXJhdGlvbl9kYXRldGltZSI6IjIwMjUtMDItMThUMDM6NDQ6MDAuODIxWiIsImFsbG93X3BlcnNpc3RlbmNlIjp0cnVlLCJyZWFsX3RpbWVfZXhwaXJhdGlvbiI6dHJ1ZX0sImNvbnRlbnRfa2V5X3VzYWdlX3BvbGljaWVzIjpbeyJuYW1lIjoiUG9saWN5IEEiLCJ3aWRldmluZSI6eyJkZXZpY2Vfc2VjdXJpdHlfbGV2ZWwiOiJTV19TRUNVUkVfQ1JZUFRPIn19XSwiY29udGVudF9rZXlzX3NvdXJjZSI6eyJpbmxpbmUiOlt7ImlkIjoiMTAyM2JhMThiZTMzYWE4YTg2MTFlZWE4OGNkZWQ5ZTMiLCJ1c2FnZV9wb2xpY3kiOiJQb2xpY3kgQSJ9LHsiaWQiOiJjY2I0ZjJmYjExNTVmN2JmYmIwN2U3Yzc1ZDI2YTY3YSIsInVzYWdlX3BvbGljeSI6IlBvbGljeSBBIn0seyJpZCI6IjU4ODE5M2VjZjhjMzc5OGY2MjMxYjM1N2M0N2YwYWJiIiwidXNhZ2VfcG9saWN5IjoiUG9saWN5IEEifV19fSwiaWF0IjoxNzM5NzY3NDQwLCJleHAiOjE3Mzk4NTM4NDB9.VhZA2UrSXAR52syUJ-deu8WNAsT65RMJDbDIdogKx4M";
+          request.headers["X-AxDRM-Message"] = "";
         }
       });
 
       const drmConfig = {
-        "com.apple.fps": "https://c8eaeae1-drm-fairplay-licensing.axprod.net/AcquireLicense",
+        "com.apple.fps": ""
       };
       playerRef.current.configure({
         drm: {
           servers: drmConfig,
           advanced: {
             "com.apple.fps": {
-              serverCertificateUri: "https://travelxp.akamaized.net/cert/fairplay/fairplay.cer",
-              getContentId: (emeOptions, initData) => {
-                return new TextDecoder().decode(initData.filter((item) => item !== 0 && item !== 150));
-              },
-            },
-          },
+              serverCertificateUri: ""
+            }
+          }
         },
+        streaming: {
+          useNativeHlsForFairPlay: true,
+          bufferingGoal: 20,
+          rebufferingGoal: 4,
+          bufferBehind: 20,
+          lowLatencyMode: false
+        }
       });
 
       try {
-        await playerRef.current.load(
-          "https://travelxp.akamaized.net/60a66806d659ded70c8f1eb6/manifest_v1_hd_28042023_1353_only_h264.m3u8"
-        );
+        await playerRef.current.load("https://travelxp.akamaized.net/60a66806d659ded70c8f1eb6/manifest_v1_hd_28042023_1353_only_h264.m3u8");
         console.log("The video has been loaded successfully!");
 
         // Get codec information from the loaded manifest
         const tracks = playerRef.current.getVariantTracks();
         if (tracks.length > 0) {
-          const codecs = tracks.map(track => track.videoCodec + ", " + track.audioCodec).join(" | ");
+          const codecs = tracks.map((track) => track.videoCodec + ", " + track.audioCodec).join(" | ");
           console.log("Codec Information:", codecs);
           setCodecInfo(codecs);
         } else {
           console.warn("No tracks found!");
         }
-
       } catch (error) {
         console.error("Error loading video", error);
       }
@@ -79,7 +79,9 @@ export default function ShakaPlayer() {
   return (
     <div>
       <video ref={videoRef} controls width="70%" height="700px" />
-      <p><strong>Codec Info:</strong> {codecInfo}</p>
+      <p>
+        <strong>Codec Info:</strong> {codecInfo}
+      </p>
     </div>
   );
 }
